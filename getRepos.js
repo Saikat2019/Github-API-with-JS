c10565b0f53ba2e6982eb8a60fc3e07a2165790e
saikat <saikatmondal410@gmail.com>
2019-02-08 23:40:30 +0530

    a simple repo on how to use github api with simple Javascript and JS

---
+++ getRepos.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+      
+$("#repos_btn").click(function() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.github.com/users/Saikat2019/repos?per_page=100&type=owner",  //dont forget to put repos?per_page=100
+        dataType: "json",                                                       //because in github page there are 30 repos 
+        success: function(result) {                                             //listed per page    
+            for( i in result ) {
+                $("#repos_list").append(
+                    "<li><a href='" + result[i].html_url + "' target='_blank'>" +
+                    result[i].name + "</a></li>"
+                );
+            }
+            $("#repos_no").append("Total Repositories: " + result.length);
+        }
+    });
+});
+
+    });
